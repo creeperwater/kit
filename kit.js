4f47b397c6d28b26cbf0871f1a81bc984e3a521b
苦力怕水 <creeperwater@163.com>
2022-12-02 20:07:33 +0800

    no version anymore

--- kit.js
+++ kit.js
@@ -1,3 +1,4 @@
+console.log('Personal website: https://klps.top \nGithub address: https://github.com/creeperwater \nFocus on Twitter: https://twitter.com/624h2o \n%c Forward Forever ','background-color:#1772b4;color:#fff;font-size:20px;');
 ((win)=>{
   'use strict';
   win.$=function(input,type=true){
@@ -8,14 +9,14 @@
       return output[0];
     else
       return output;
-  }
+  };
   win.atr=function(input,name,value=false){
     if(value){
       input.setAttribute(name,value);
     }else{
       return input.getAttribute(name);
     }
-  }
+  };
   win.get=function(url){
     return new Promise(function(resolve,reject){
       let get = new XMLHttpRequest();
@@ -23,29 +24,28 @@
         if(this.status==200){
           resolve(this.responseText);
         }else{
-          reject(this.statusText);
+          reject(this.responseText);
         }
       };
       get.open('GET',url);
       get.send();
     });
-  }
+  };
   win.loc=function(name,value=false){
     if(value){
       localStorage.setItem(name,value);
     }else{
       return localStorage.getItem(name);
     }
-    //localStorage.removeItem('myCat');localStorage.clear();
-  }
+  };
   win.$.show=function(key){
     $(key,false).forEach(ele=> {
       ele.style.display=null;
     });
-  }
+  };
   win.$.hide=function(key){
     $(key,false).forEach(ele=> {
       ele.style.display='none';
     });
-  }
+  };
 })(window);
